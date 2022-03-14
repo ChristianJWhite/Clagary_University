37e3f0726e9315122e387c7b37c4b8141edf2a35
Christian White <98524388+ChristianJWhite@users.noreply.github.com>
2022-03-14 20:53:04 +1300

    Add files via upload

---
+++ Calgary_University.py
@@ -0,0 +1,144 @@
+from datetime import datetime
+
+class Parser:
+    def __init__(self):
+        self.numberOfDays = 0 # Count number of days passed
+        
+        self.startDate = datetime.today()
+        self.endDate = datetime.today()
+        
+        self.fileTypeDict = {} # Contains file extension - file type information
+        self.initializeFileType()
+        
+    def initializeFileType(self):  # Defines file types for each file
+        self.fileTypeDict["html"] = "HTML"
+        self.fileTypeDict["htm"] = "HTML"
+        self.fileTypeDict["shtml"] = "HTML"
+        self.fileTypeDict["map"] = "HTML"
+
+        self.fileTypeDict["gif"] = "Images"
+        self.fileTypeDict["jpeg"] = "Images"
+        self.fileTypeDict["jpg"] = "Images"
+        self.fileTypeDict["xbm"] = "Images"
+        self.fileTypeDict["bmp"] = "Images"
+        self.fileTypeDict["rgb"] = "Images"
+        self.fileTypeDict["xpm"] = "Images"
+
+        self.fileTypeDict["au"] = "Sound"
+        self.fileTypeDict["snd"] = "Sound"
+        self.fileTypeDict["wav"] = "Sound"
+        self.fileTypeDict["mid"] = "Sound"
+        self.fileTypeDict["midi"] = "Sound"
+        self.fileTypeDict["lha"] = "Sound"
+        self.fileTypeDict["aif"] = "Sound"
+        self.fileTypeDict["aiff"] = "Sound"
+
+        self.fileTypeDict["mov"] = "Video"
+        self.fileTypeDict["movie"] = "Video"
+        self.fileTypeDict["avi"] = "Video"
+        self.fileTypeDict["qt"] = "Video"
+        self.fileTypeDict["mpeg"] = "Video"
+        self.fileTypeDict["mpg"] = "Video"
+
+        self.fileTypeDict["ps"] = "Formatted"
+        self.fileTypeDict["eps"] = "Formatted"
+        self.fileTypeDict["doc"] = "Formatted"
+        self.fileTypeDict["dvi"] = "Formatted"
+        self.fileTypeDict["txt"] = "Formatted"
+
+        self.fileTypeDict["cgi"] = "Dynamic"
+        self.fileTypeDict["pl"] = "Dynamic"
+        self.fileTypeDict["cgi-bin"] = "Dynamic"
+
+    def parse(self, logFile):  # Read each line from the log and process output
+        index = 0
+        typeDict = {}
+        totalData = 0
+        freqDict = {}
+        for line in logFile:
+            elements = line.split()
+
+            # Skip to the next line if this line has an empty string
+            if line is '':continue
+
+            # Skip to the next line if this line contains not equal to 9 - 11 elements
+            if not (9 <= len(elements) <= 11):continue
+
+            # Corrects a record with a single "-"
+            if (len(elements) == 9 and elements[2] != '-'):
+                elements.insert(2, '-')
+
+            sourceAddress = elements[0]
+            timeStr = elements[3].replace('[', '')
+            requestMethod = elements[5]
+            requestFileName = elements[6].replace('"', '')
+            responseCode = elements[len(elements) - 2]
+            replySizeInBytes = elements[len(elements) - 1]
+
+            #########################################################################
+
+            if replySizeInBytes != "-":
+                dataBytes = int(replySizeInBytes)
+            else:
+                dataBytes = 0
+
+            month = str(timeStr[3] + timeStr[4] + timeStr[5])
+
+            #File Types#
+            time = (timeStr[12] + timeStr[13] + timeStr[14] + timeStr[15] + timeStr[16] + timeStr[17] + timeStr[18] + timeStr[19])
+            fileType = self.getFileType(requestFileName)
+            #print(day)
+            if str(responseCode) == "200":
+                if requestFileName not in typeDict.keys():
+                    typeDict.setdefault(requestFileName, [])   #adds into dictionary
+                    typeDict[requestFileName].append(time)
+                else:
+                    typeDict[requestFileName].append(time)
+
+            #if responeCode == 200:
+
+
+            totalData += dataBytes  #7,946,268,208 bytes
+
+
+
+
+            # Prints assigned elements.
+            #print('{0} , {1} , {2} , {3} , {4} , {5} '.format(sourceAddress,timeStr,requestMethod,requestFileName,responseCode, replySizeInBytes),end="\n")
+            
+
+            #Start date and an end date.
+            self.startDate = datetime.strptime(timeStr, "%d/%b/%Y:%H:%M:%S")
+            self.endDate = datetime.strptime(timeStr, "%d/%b/%Y:%H:%M:%S")
+
+        ####################################################  
+        ####### Write code below to generate outputs #######
+
+        print(typeDict['10914.gif'])    #Shows the times the gif 10914 was accessed
+
+        uniqueObject = 0
+        oneObject = 0
+        
+
+    def getFileType(self, URI):
+        if URI.endswith('/') or URI.endswith('.') or URI.endswith('..'):
+            return 'HTML'
+        filename = URI.split('/')[-1]
+        if '?' in filename:
+            return 'Dynamic'
+        extension = filename.split('.')[-1].lower()
+        if extension in self.fileTypeDict:
+            return self.fileTypeDict[extension]
+        return 'Others'
+
+    def checkResCode(self, code):
+        if code == '200' : return 'Successful'
+        if code == '302' : return 'Found'
+        if code == '304' : return 'Not Modified'   
+        return None
+
+if __name__ == '__main__':
+    logfile = open('access_log', 'r', errors='ignore')
+    logParser = Parser()
+    logParser.parse(logfile)
+    pass
